274194dab0b4630235a7815911e1081f9bb3a9cc
Roberto <robertohgm@hotmail.com>
2021-10-19 17:55:04 -0300

    Primeiro commit

---
+++ src/DemoAzureAd.Api/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAzureAd.Api.Controllers
+{
+    [Authorize]
+    [RequiredScope("tasks.read")]
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor contextAccessor)
+        {
+            _logger = logger;
+            _contextAccessor = contextAccessor;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new { name = User.Identity.Name });
+        }
+    }
+}
---
+++ src/DemoAzureAd.Api/Controllers/PublicController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAzureAd.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PublicController : ControllerBase
+    {
+        private readonly ILogger<PublicController> _logger;
+
+        public PublicController(ILogger<PublicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new { date = DateTime.UtcNow.ToString() });
+        }
+    }
+}
